38e38bf8a1a3c63f06bfaab702e449040e8ab25d
nandinibaride <nandinibaride1234@gmail.com>
2021-08-18 23:50:56 +0530

    Added: calculated percentage of head & tail

--- src/main/java/basicprograms/com/FlipCoin.java
+++ src/main/java/basicprograms/com/FlipCoin.java
@@ -1,10 +1,37 @@
 package basicprograms.com;
+import java.util.*;
 
 public class FlipCoin {
 	public static void main(String args[]) {
 		
-		 System.out.println("Welcome");
+		 int i = 1 ;
+		 int Head = 0 ;
+		 int Tail = 0 ;
+		 
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter The number of time to filp the coin");
+		 int num=sc.nextInt();
+		 
+				while (i <= num) {
+					double randomnum=Math.random();
+					if (randomnum < 0.5) {
+					  Tail++;
+		 			}
+					else {
+		  			Head++;
+					}
+				   i++;
+				    }
+				System.out.println("Head = "+Head);
+				System.out.println("Tail = "+Tail);
+
+		        int HeadPercentage=(Head*100)/num;
+				int TailPercentage=(Tail*100)/num;
+				System.out.println("Head Percentage = "+HeadPercentage + "%");
+		                System.out.println("Tail Percentage = "+TailPercentage + "%");
+		        sc.close();
+		        }
 		 
 	}
 	
-}	
+	
