515d7305ca3a59c2fad47a333090ca6c4be7e493
nandinibaride <nandinibaride1234@gmail.com>
2021-08-19 13:46:50 +0530

    Added: Power of Two

---
+++ src/main/java/basicprograms/com/PowerOfTwo.java
@@ -0,0 +1,10 @@
+package basicprograms.com;
+
+public class PowerOfTwo {
+	public static void main(String args[]) {
+		int N = 1;
+		for(N=1;N<=31;N++) {
+			System.out.println(Math.pow (2 , N));
+		}
+	 }
+}
