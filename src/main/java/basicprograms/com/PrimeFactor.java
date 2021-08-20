24c053bdc6eccc8755c7c80820378403a7073a6d
nandinibaride <nandinibaride1234@gmail.com>
2021-08-20 19:46:49 +0530

    Added: Calculate Prime factors

---
+++ src/main/java/basicprograms/com/PrimeFactor.java
@@ -0,0 +1,22 @@
+package basicprograms.com;
+
+import java.util.Scanner;
+
+public class PrimeFactor {
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The number");
+		int num=sc.nextInt();
+		 for(int i=2;i<num;i++) {
+			 if(num%i == 0) {
+		            System.out.println(i);
+		            num = num/i;
+		         }
+		      }
+		      if(num >2) {
+		       System.out.println(num);
+			}
+	}	      
+}
+ 
